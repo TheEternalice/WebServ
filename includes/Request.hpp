9b5d1b169873dfe96d2daacdfd3cfd44a21fc3d8
TheEternalice <wsl@DESKTOP-2G2EKDB.localdomain>
2025-09-03 23:36:32 +0200

    feat(Request): parsing of HTTP request added

--- includes/Request.hpp
+++ includes/Request.hpp
@@ -3,29 +3,47 @@
 /*                                                        :::      ::::::::   */
 /*   Request.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lde-merc <lde-merc@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ade-rese <ade-rese@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/14 16:26:29 by lde-merc          #+#    #+#             */
-/*   Updated: 2025/08/14 16:28:07 by lde-merc         ###   ########.fr       */
+/*   Updated: 2025/09/03 23:26:35 by ade-rese         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <string>
+#include <map>
+#include <sstream>
+#include <stdexcept>
+#include <algorithm>
 
 class Request {
 	public:
 		Request();
+		Request(const std::string &request);
 		~Request();
 		Request(const Request &other);
 		
 		Request &operator=(const Request &other);
 
 		std::string get_content_type(const std::string &path);
+		void parse(const std::string &buffer);
+
+		const std::string &getMethod() const;
+		const std::string &getUrl() const;
+		const std::string &getHttpVersion() const;
+		const std::string &getBody() const;
 		
+
 	private:
-		char _method[8];
-		char _url[1024];
-		std::string _type;
+		std::string _method;       // GET, POST, DELETE, etc.
+		std::string _url;          // /index.html, /script.py
+		std::string _http_version; // HTTP/1.1
+		std::map<std::string, std::string> _headers; // "Host" -> "localhost:8080"
+		std::string _body;         // Corps de la requête (POST data)
+
+		// Pour le parsing progressif si tu utilises recv non bloquant
+		std::string buffer;       // tampon où tu accumules ce que tu lis
+		bool complete; 
 };
--- srcs/Request.cpp
+++ srcs/Request.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Request.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lde-merc <lde-merc@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ade-rese <ade-rese@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/14 16:26:26 by lde-merc          #+#    #+#             */
-/*   Updated: 2025/08/14 16:28:18 by lde-merc         ###   ########.fr       */
+/*   Updated: 2025/09/03 23:35:12 by ade-rese         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 // Constructeur
 Request::Request() {}
+
+Request::Request(const std::string &request) {parse(request);}
 
 Request::~Request() {}
 
@@ -28,6 +30,54 @@ Request &Request::operator=(const Request &other) {
     return *this;
 }
 
+const std::string &Request::getMethod() const {
+	return (_method);
+}
+
+const std::string &Request::getUrl() const {
+	return (_url);
+}
+
+const std::string &Request::getHttpVersion() const {
+	return (_http_version);
+}
+
+const std::string &Request::getBody() const {
+	return (_body);
+}
+
+// "GET /uri.cgi HTTP/1.1\r\n"
+// "User-Agent: Mozilla/5.0\r\n"
+// "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n"
+// "Host: 127.0.0.1\r\n"
+// "\r\n";
+
+void Request::parse(const std::string &buffer) {
+	size_t	pos = buffer.rfind("\r\n\r\n");
+	if (pos == std::string::npos) {
+		throw (std::runtime_error("Invalid HTTP request"));
+	}
+	std::string header = buffer.substr(0, pos);
+	std::string body = buffer.substr(pos + 4);
+	std::istringstream stream(header);
+	std::string line;
+	if (!std::getline(stream, line))
+		throw (std::runtime_error("Empty HTTP request"));
+	if (line.back() == '\r') line.pop_back();
+	std::istringstream first_line(line);
+	first_line >> _method >> _url >> _http_version;
+	while (std::getline(stream, line)) {
+		if (line.back() == '\r') line.pop_back();
+		if (line.empty()) break;
+		size_t colon = line.find(":");
+		if (colon == std::string::npos) continue;
+		std::string key = line.substr(0, colon);
+		std::string value = line.substr(colon + 1);
+		_headers[key] = value;
+	}
+	if (size_t len = _method.find("POST")) {_body = body.substr(0, len);}
+	// géré les chunked body ??????
+}
 
 std::string Request::get_content_type(const std::string &path) {
 	if (path.size() >= 5 && path.substr(path.size()-5) == ".html") return "text/html";
