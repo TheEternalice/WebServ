46a7b39d9e5620e1845ed262292d2d9a92d027fb
Gabriel Pichon <gpichon@f6r2s10.clusters.42paris.fr>
2025-11-18 14:00:52 +0100

    fixed(server_parsing, server, Reponse) change the execution method of request (with map) max_body_size, change the root configuration, now working with cgi(need to test withouthard url in hatml)

--- objs/Request.d
+++
@@ -1,3 +0,0 @@
-objs/Request.o: srcs/Request.cpp srcs/../includes/Request.hpp \
-  srcs/../includes/Reponse.hpp srcs/../includes/Server.hpp \
-  srcs/../includes/Utils.hpp srcs/../includes/Client.hpp
