cc24a98cdf54e9256829df83712e794d266ae3d9
Gebz <148749394+Gebzof@users.noreply.github.com>
2025-11-28 11:55:46 +0100

    fixed(server.cpp) upload file/mesage, and jump on the redirection up

--- objs/Server.d
+++
@@ -1,3 +0,0 @@
-objs/Server.o: srcs/Server.cpp srcs/../includes/Server.hpp \
- srcs/../includes/Request.hpp srcs/../includes/Reponse.hpp \
- srcs/../includes/Utils.hpp srcs/../includes/Client.hpp
--- objs/Server_parsing.d
+++
@@ -1,4 +0,0 @@
-objs/Server_parsing.o: srcs/Server_parsing.cpp \
- srcs/../includes/Server.hpp srcs/../includes/Request.hpp \
- srcs/../includes/Reponse.hpp srcs/../includes/Utils.hpp \
- srcs/../includes/Client.hpp
